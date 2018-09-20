af03b617a9f01fd6d84529654b59f53538c58fed
Gautam <gautamhans1@gmail.com>
2018-09-21 00:03:14 +0530

    initial commit

---
+++ Readme.md
@@ -0,0 +1,3 @@
+![Travis-ci](https://api.travis-ci.org/gautamhans1/QuizApp.svg)
+
+A
---
+++ app/src/main/java/com/example/gautam/quizapp/QuizActivity.java
@@ -0,0 +1,145 @@
+package com.example.gautam.quizapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class QuizActivity extends AppCompatActivity {
+
+    private ImageButton mNextButton;
+    private ImageButton mPreviousButton;
+
+    private Button mTrueButton;
+    private Button mFalseButton;
+
+    private Toast mToast;
+    private TextView mQuestionText;
+
+    private static final String TAG = "QuizActivity";
+
+    private Question[] mQuestionBank = new Question[]{
+            new Question(R.string.question_africa, false),
+            new Question(R.string.question_americas, true),
+            new Question(R.string.question_asia, true),
+            new Question(R.string.question_australia, true),
+            new Question(R.string.question_mideast, false),
+            new Question(R.string.question_oceans, true)
+    };
+
+    private int mCurrentIndex = 0;
+    private int mPreviousIndex = 0;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate(Bundle) called");
+        setContentView(R.layout.activity_quiz);
+
+        mTrueButton = findViewById(R.id.btnTrue);
+        mFalseButton = findViewById(R.id.btnFalse);
+        mQuestionText = findViewById(R.id.question_text);
+        mNextButton = findViewById(R.id.btnNext);
+        mPreviousButton = findViewById(R.id.btnPrevious);
+
+        int question = mQuestionBank[mCurrentIndex].getTextResId();
+        mQuestionText.setText(question);
+
+        mQuestionText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateQuestion();
+            }
+        });
+
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkAnswer(true);
+            }
+        });
+
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkAnswer(false);
+            }
+        });
+
+        mPreviousButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex = mPreviousIndex;
+                updateQuestion();
+            }
+        });
+
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mPreviousIndex = mCurrentIndex;
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                updateQuestion();
+            }
+        });
+
+        updateQuestion();
+    }
+
+    private void updateQuestion() {
+        int question = mQuestionBank[mCurrentIndex].getTextResId();
+        mQuestionText.setText(question);
+    }
+
+    private void checkAnswer(boolean userPressedTrue) {
+        boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
+
+        int messageResId = 0;
+
+        if (userPressedTrue == answerIsTrue) {
+            messageResId = R.string.correct_toast;
+        } else {
+            messageResId = R.string.incorrect_toast;
+        }
+
+        if (mToast != null) mToast.cancel();
+        mToast = Toast.makeText(QuizActivity.this, messageResId, Toast.LENGTH_SHORT);
+        mToast.show();
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
+    }
+}
